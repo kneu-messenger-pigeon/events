7ae713b213b6dc15fb708f4077a3bb4a0a216db6
Anton Berezhnyi <aberezhniy@clips4sale.com>
2023-04-22 22:16:07 +0300

    Add extra fields for UserAuthorizedEvent

--- authorized_users.go
+++ authorized_users.go
@@ -1,9 +1,57 @@
 package events
 
+import (
+	"encoding/json"
+)
+
 const UserAuthorizedEventName = "UserAuthorizedEvent"
 
 type UserAuthorizedEvent struct {
 	Client       string
 	ClientUserId string
 	StudentId    int
+
+	LastName   string `json:"last_name"`
+	FirstName  string `json:"first_name"`
+	MiddleName string `json:"middle_name"`
+	Gender     Gender `json:"gender"`
+}
+
+type Gender int8
+
+const (
+	UnknownGender Gender = iota
+	Male
+	Female
+)
+
+var GenderToStringMap = map[Gender]string{
+	UnknownGender: "",
+	Male:          "male",
+	Female:        "female",
+}
+
+func (gender *Gender) String() string {
+	return GenderToStringMap[*gender]
+}
+
+// MarshalJSON marshals the enum as a quoted json string
+func (gender *Gender) MarshalJSON() ([]byte, error) {
+	return json.Marshal(gender.String())
+}
+
+// UnmarshalJSON unmashals a quoted json string to the enum value
+func (gender *Gender) UnmarshalJSON(b []byte) error {
+	var genderJsonString string
+	err := json.Unmarshal(b, &genderJsonString)
+	if err != nil {
+		for GenderCandidate, GenderCandidateString := range GenderToStringMap {
+			if GenderCandidateString == genderJsonString {
+				*gender = GenderCandidate
+				break
+			}
+		}
+	}
+
+	return err
 }
